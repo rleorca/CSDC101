c7217e0139225279554b2d446f834361e3d724d3
rleorca <rleorca@gbox.adnu.edu.ph>
2025-08-08 14:21:25 +0800

    Create leorcamain.cpp

---
+++ leorcamain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>// input and output library 
+using namespace std;// standard namespace for cin and cout 
+
+int main (){
+    
+    int num1,num2,sum;
+    
+    cout<<"Enter a Number: \n";// output enter number
+    cin>>num1;// user input number
+    cout<< "Enter Another Number: ";// output enter number
+    cin>>num2;// user input enter number
+    sum = num1 + num2;// set the value of the sum1 + sum2 ;
+    cout<<" the sum of the two number is: "<<sum;// output of the number
+    
+     return 0;
+}
