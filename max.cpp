79d93602257d5a4fa3c28a7153646ad0d1717d0b
rleorca <rleorca@gbox.adnu.edu.ph>
2025-10-24 13:15:46 +0800

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+ int grade[4] = {90, 95, 100, 85};
+ int max = grade[0];
+ 
+ for(grade[i] > max)
+ max = grade[i];
+ 
+ }
+ cout<< max;
+ 
+ return 0;
+ }
+ 
