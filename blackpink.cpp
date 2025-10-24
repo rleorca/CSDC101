9a218450cd56a6242343c2273c3525a9a5be7bf4
rleorca <rleorca@gbox.adnu.edu.ph>
2025-10-24 13:01:59 +0800

    Add files via upload

---
+++ blackpink.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+ string blackpink[4]= {"Lisa","Jisoo","Jennie","Rose",};
+ 
+ for(int i = 0; i <4; i++){
+ cout<< "the no." << i <<"member of blackpink is " << blackpink[i]<< endl;
+ }
+ 
+ return 0;
+ }
---
+++ shoes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int cars[5]={1, 23, 45, 44, 2};
+ 
+ cout<<" the first shoes is " << cars[0] << endl;
+ cout<<" the second shoes is " << cars[1] << endl;
+ cout<<" the third shoes is " << cars[2] << endl;
+ cout<<" the fourth shoes is " << cars[3] << endl;
+ cout<<" the fifth shoes is " << cars[4] << endl;
+
+
+return 0;
+}
